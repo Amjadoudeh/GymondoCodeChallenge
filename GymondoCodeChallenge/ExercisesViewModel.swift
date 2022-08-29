cb9c6d93601aa9214f330b9d3294ee8df7343b3c
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-29 12:54:43 +0200

    GCC-3: Create ExerciseDetailsViewModel

--- GymondoCodeChallenge/ExercisesViewModel.swift
+++ GymondoCodeChallenge/ExercisesViewModel.swift
@@ -7,6 +7,12 @@ enum ExercisesState {
     case error
 }
 
+enum ExerciseDetailState {
+    case loading
+    case loaded(exercies: [Exercise])
+    case error
+}
+
 class ExercisesViewModel: ObservableObject {
     @Published var state: ExercisesState = .loading
     var cancellables = Set<AnyCancellable>()
@@ -22,5 +28,25 @@ class ExercisesViewModel: ObservableObject {
     func loadExercises() {
         state = .loading
         self.exerciseService.fetchExcercises()
+    }
+    
+}
+
+
+class ExerciseDetailsViewModel: ObservableObject {
+    @Published var state: ExerciseDetailState = .loading
+    var cancellables = Set<AnyCancellable>()
+    private var exerciseService: ExerciseManager
+    init(exerciseService: ExerciseManager) {
+        self.exerciseService = exerciseService
+
+        self.exerciseService.$exercises.sink {
+            self.state = .loaded(exercies: $0)
+        }.store(in: &cancellables)
+    }
+
+    func loadExerciseImages(exerciseID: Int) {
+        state = .loading
+        self.exerciseService.fetchExcerciseImages(exerciseID: exerciseID)
     }
 }
