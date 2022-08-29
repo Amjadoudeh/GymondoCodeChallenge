97d52cf7a300680326517a62b526eac773500c6c
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-08-29 07:50:28 +0200

    GCC-3: Style the ExCardDetailsView

--- GymondoCodeChallenge/Views/Details/ExerciseDetailsCradView.swift
+++ GymondoCodeChallenge/Views/Details/ExerciseDetailsCradView.swift
@@ -1,15 +1,42 @@
-//
-//  ExerciseDetailsCradView.swift
-//  GymondoCodeChallenge
-//
-//  Created by Amjad Oudeh on 29.08.22.
-//
-
 import SwiftUI
 
 struct ExerciseDetailsCradView: View {
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // MARK: Exercise title
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .font(.title.weight(.medium))
+            Spacer()
+            // MARK: Exercise description
+            Text("in eine aufrechte Position setzen, bei der die Beine hüftbreit geöffnet sind den Rücken, die Knie und die Oberschenkelaußenseite an die jeweiligen Polster pressen mit den Händen an den Griffen festhalten die Oberschenkel bis zum Anschlagpunkt der Hebelarme auseinander drücken danach die Beine wieder zusammen führen das Gesäß nicht vom Sitz abheben beim Auseinanderdrücken der Hebelarme aus- und beim Zusammenführen einatme")
+                .font(.callout)
+                .multilineTextAlignment(.leading)
+                .lineLimit(.none)
+            Spacer()
+            // MARK: Exercise images
+            ScrollView(.horizontal, showsIndicators: true) {
+                HStack {
+                    ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { _ in
+                        Image("sn")
+                            .resizable()
+                            .cornerRadius(5)
+                        .frame(width: 100, height: 100)
+                    }
+                }
+            }
+            Spacer()
+            // MARK: Exercise variations
+            NavigationLink {
+
+            } label: {
+
+            }
+
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+
     }
 }
 
